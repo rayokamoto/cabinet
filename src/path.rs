303f3bf0568c1887b3a176764fe843fc37848c8c
Ray <22254748+rayokamoto@users.noreply.github.com>
2022-04-17 00:55:33 +0930

    Added more directories

--- src/path.rs
+++ src/path.rs
@@ -18,23 +18,15 @@ pub fn get_path(path: &String, path_type: ArgumentType) -> Option<PathBuf> {
     let mut path_ref: Option<PathBuf> = None;
 
     if path_type == ArgumentType::Template {
-        let path = path.to_lowercase();
-        
-        // TODO: add more directories
-        if path == String::from("home") {
-            path_ref = dirs::home_dir();
-        }
-        else if path == String::from("desktop") {
-            path_ref = dirs::desktop_dir();
-        }
-        else if path == String::from("downloads") {
-            path_ref = dirs::download_dir();
-        }
-        else if path == String::from("documents") {
-            path_ref = dirs::document_dir();
-        }
-        else {
-            println!("The template '{}' does not exist.", &path);
+        let path = &path.to_lowercase()[..];
+        match path {
+            "audio" | "music" => path_ref = dirs::audio_dir(),
+            "documents" => path_ref = dirs::document_dir(),
+            "desktop" => path_ref = dirs::desktop_dir(),
+            "downloads" => path_ref = dirs::download_dir(),
+            "home" => path_ref = dirs::home_dir(),
+            "pictures" => path_ref = dirs::picture_dir(),
+            _ => println!("The template '{}' does not exist.", &path)
         }
     }
     else if path_type == ArgumentType::Path {
