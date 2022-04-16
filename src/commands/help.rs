50931b024283b2744df2c2526ac73c53777c7f06
Ray <22254748+rayokamoto@users.noreply.github.com>
2022-04-16 22:44:26 +0930

    Added command modules

---
+++ src/commands/help.rs
@@ -0,0 +1,14 @@
+
+fn exec() {
+    print!("Cabinet 0.1.0
+Usage: cab <command> [<options>] <path>
+
+Available commands:
+   help      Shows this command
+   type      Sort files by file type
+   name      Sort files by file name
+   date      Sort files by their date of modification
+
+Use -h or --help for more information on a command. 
+")
+}
