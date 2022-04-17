c1c8b079be1f2851fe9327c7a3617e2994d66a3d
Ray <22254748+rayokamoto@users.noreply.github.com>
2022-04-17 21:05:15 +0930

    Updated command help

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,5 +1,5 @@
 //use crate::parser;
-use crate::parser::SubCommand;
+//use crate::parser::SubCommand;
 
 pub fn help() {
     print!("Cabinet 0.1.0
@@ -15,28 +15,40 @@ Use -h or --help for more information on a command.
 ")
 }
 
+// TODO: SubCommand::<subcommand> or use of string??
 
-pub fn cmd_help(command: SubCommand) {
+pub fn cmd_help<S: AsRef<str>>(command: S) {
+    let command = command.as_ref();
 
-    if command == SubCommand::Type {
+
+    if command == "type" {
         print!("command: type
 Usage: cab type [<options>] <path>
     -p, --path      The path you are using is an absolute path
     -t, --template  The path you are using is a predefined one. E.g. downloads for your downloads folder
 ")
     }
-    else if command == SubCommand::Name {
+    else if command == "name" {
         print!("command: name
 Usage: cab name [<options>] <path>
-    
+    -p, --path          The path you are using is an absolute path
+    -t, --template      The path you are using is a predefined one. E.g. downloads for your downloads folder
+    --includes=<match>  File name includes...
+    --excludes=<match>  File name excludes...
+\nWARNING: CURRENTLY NOT IMPLEMENTED!
 ")
     }
-    else if command == SubCommand::Date {
+    else if command == "date" {
         print!("command: date
 Usage: cab date [<options>] <path>
-
+    -p, --path      The path you are using is an absolute path
+    -t, --template  The path you are using is a predefined one. E.g. downloads for your downloads folder
+    --before=<date> Get files from before specified date. Date format is YYYY-MM-DD
+    --after=<date>  Get files from after specified date. Date format is YYYY-MM-DD
+\nWARNING: CURRENTLY NOT IMPLEMENTED!
 ")
     }
+    // This should never be called
     else {
         println!("The command either does not exist or no documentation currently exists for it.");
     }
