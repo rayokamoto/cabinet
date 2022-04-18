4fe93ba145423d07285532eae47453b380c8bb36
Ray <22254748+rayokamoto@users.noreply.github.com>
2022-04-18 13:25:58 +0930

    Make a start on file size command

---
+++ src/commands/file_size.rs
@@ -0,0 +1,8 @@
+
+use crate::parser::Token;
+use crate::path::{get_path, ArgType};
+
+pub fn file_size(args: &Vec<Token>) {
+    // NOT IMPLEMENTED 
+    println!("ERROR: Currently not implemented.");
+}
