de04ed411fcb6240a9c37c58665c292721ced38e
Ray <22254748+rayokamoto@users.noreply.github.com>
2023-02-19 15:18:53 +1030

    feat: Add regex option for name sorting

--- src/commands/name.rs
+++ src/commands/name.rs
@@ -3,6 +3,7 @@ use std::fs::{self, DirEntry};
 use std::path::PathBuf;
 
 use clap::{Arg, ArgMatches, Command};
+use regex::Regex;
 
 use crate::util;
 use crate::util::path::get_path;
@@ -23,6 +24,12 @@ pub fn cli() -> Command {
                 .value_name("match")
                 .help("File name excludes...")
                 .action(clap::ArgAction::Set),
+            Arg::new("regex")
+                .short('R')
+                .long("regex")
+                .value_name("match")
+                .help("Use regular expressions (regex) for pattern matching")
+                .action(clap::ArgAction::SetTrue),
             Arg::new("template")
                 .short('t')
                 .long("template")
@@ -53,6 +60,7 @@ pub fn exec(args: &ArgMatches) {
 
     let mut include_pattern: Option<String> = None;
     let mut exclude_pattern: Option<String> = None;
+    let use_regex = args.get_flag("regex");
 
     if let Some(incl) = args.get_one::<String>("includes") {
         include_pattern = Some(incl.to_string());
@@ -67,20 +75,6 @@ pub fn exec(args: &ArgMatches) {
         return;
     }
 
-    let mut has_include = false;
-    let mut has_exclude = false;
-    let mut include = String::new();
-    let mut exclude = String::new();
-
-    if include_pattern != None {
-        has_include = true;
-        include = include_pattern.unwrap();
-    }
-    if exclude_pattern != None {
-        has_exclude = true;
-        exclude = exclude_pattern.unwrap();
-    }
-
     let dir = fs::read_dir(path.as_ref().unwrap()).unwrap();
     let paths_parent = path.as_ref().unwrap().display().to_string();
     let parent = path.unwrap();
@@ -88,22 +82,84 @@ pub fn exec(args: &ArgMatches) {
 
     let mut files: Vec<DirEntry> = vec![];
 
-    for item in dir {
-        let item = item.unwrap();
-        let md = item.metadata().unwrap();
+    if use_regex {
+        let mut has_include = false;
+        let mut has_exclude = false;
+        let mut include: Regex = Regex::new("").unwrap();
+        let mut exclude: Regex = Regex::new("").unwrap();
 
-        let filename = &item.file_name(); // gets the name (no file extension)
-        let f = OsStr::to_str(&filename).unwrap();
+        if include_pattern != None {
+            has_include = true;
+            include = Regex::new(include_pattern.unwrap().as_str()).unwrap();
+        }
+        if exclude_pattern != None {
+            has_exclude = true;
+            exclude = Regex::new(exclude_pattern.unwrap().as_str()).unwrap();
+        }
 
-        if md.is_file() {
-            if (has_include && has_exclude) && (f.contains(&include) && !f.contains(&exclude)) {
-                files.push(item);
-            } else if (has_include && !has_exclude) && f.contains(&include) {
-                files.push(item);
+        for item in dir {
+            let item = item.unwrap();
+            let md = item.metadata().unwrap();
+
+            let filename = &item.file_name();
+            let f = OsStr::to_str(&filename).unwrap();
+
+            if md.is_file() {
+                if has_include && has_exclude {
+                    match include.captures(f) {
+                        Some(_) => {}
+                        None => continue,
+                    };
+                    match exclude.captures(f) {
+                        Some(_) => {}
+                        None => continue,
+                    };
+                    files.push(item);
+                } else if has_include && !has_exclude {
+                    match include.captures(f) {
+                        Some(_) => {}
+                        None => continue,
+                    };
+                    files.push(item);
+                } else if has_exclude && !has_include {
+                    match exclude.captures(f) {
+                        Some(_) => {}
+                        None => continue,
+                    };
+                    files.push(item);
+                }
             }
-            // Only add file if it DOESN'T include the pattern (since it is exclude pattern)
-            else if (has_exclude && !has_include) && !f.contains(&exclude) {
-                files.push(item);
+        }
+    } else {
+        let mut has_include = false;
+        let mut has_exclude = false;
+        let mut include = String::new();
+        let mut exclude = String::new();
+
+        if include_pattern != None {
+            has_include = true;
+            include = include_pattern.unwrap();
+        }
+        if exclude_pattern != None {
+            has_exclude = true;
+            exclude = exclude_pattern.unwrap();
+        }
+
+        for item in dir {
+            let item = item.unwrap();
+            let md = item.metadata().unwrap();
+
+            let filename = &item.file_name(); // gets the name (no file extension)
+            let f = OsStr::to_str(&filename).unwrap();
+
+            if md.is_file() {
+                if (has_include && has_exclude) && (f.contains(&include) && !f.contains(&exclude)) {
+                    files.push(item);
+                } else if (has_include && !has_exclude) && f.contains(&include) {
+                    files.push(item);
+                } else if (has_exclude && !has_include) && !f.contains(&exclude) {
+                    files.push(item);
+                }
             }
         }
     }
